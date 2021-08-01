f58ffa1740b8bee1b9fa74623e7dd73abc8c871e
Manmeet4046 <rainaermanmeet@gmail.com>
2021-08-01 17:47:27 +0530

    ankit files

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,34 @@
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="copyright-text">
+                        <p>Copyright &copy; 2017 Company Name 
+                        
+                        - Design: <a href="http://www.tooplate.com/view/2089-meteor" target="_parent">sagacious E-systems</a></p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-icons">
+                        <ul>
+                            <li>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-rss"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Meteor HTML CSS Template</title>
+<!--
+
+Template 2089 Meteor
+
+http://www.tooplate.com/view/2089-meteor
+
+-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/css/bootstrap-theme.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/css/fontAwesome.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/css/hero-slider.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/css/tooplate-style.css')}}">
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <style>
+           .dropdown-toggle:active, .open .dropdown-toggle {
+        background:inherit !important; 
+        box-shadow: none;
+       
+    }
+
+        </style>
+    </head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <nav class="navbar navbar-inverse" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand scroll-top">
+                        <div class="logo"></div>
+                    </a>
+                </div>
+                <!--/.navbar-header-->
+                <div id="main-nav" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" class="scroll-top">Home</a></li>
+                          <li><a href="#" class="scroll-link" data-id="services">Services</a></li>
+                        <li><a href="#" class="scroll-link" data-id="what-we-do">About Us</a></li>
+                        {{--<li class="dropdown" >
+                          <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Services <span class="caret"></span></a>
+                          <ul class="dropdown-menu ">
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Software Development</a></li>
+                            <li><a href="#">Digital Marketing</a></li>
+                            <li><a href="#">Search Engine Optimization</a></li>
+                            <li><a href="#">Pay Per Click</a></li>
+                            <li><a href="#">Social Media Marketing</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                          </ul>
+                        </li> --}}
+                      
+                        <li><a href="#" class="scroll-link" data-id="portfolio">Our Work</a></li>
+                        <li><a href="#" class="scroll-link" data-id="blog">Blog</a></li>
+                        
+                        
+                        
+                        <li><a href="#" class="scroll-link" data-id="contact">Contact Us</a></li>
+                    </ul>
+                </div>
+                <!--/.navbar-collapse-->
+            </nav>
+            <!--/.navbar-->
+        </div>
+        <!--/.container-->
+    </div>
+    <!--/.header-->
